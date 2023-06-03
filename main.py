233a1f144862de1fa42d12561be073f8de4422c5
radeeyate <124477460+radeeyate@users.noreply.github.com>
2023-06-03 11:51:13 -0600

    make it better (add checking of status codes)

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import requests
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory = "templates")
+redirStatusCodes = (301, 302)
 
 @app.get("/")
 async def home(request: Request):
@@ -13,5 +14,6 @@ async def home(request: Request):
 
 @app.post("/")
 async def check(request: Request, url: str = Form()):
-    redirectedURL = requests.get(url).url
-    return templates.TemplateResponse("checked.html", {"request": request, "url": redirectedURL})
+    redirectedURL = requests.get(url)
+    print(redirectedURL.status_code)
+    return templates.TemplateResponse("checked.html", {"request": request, "redirURL": redirectedURL, "url": url})
